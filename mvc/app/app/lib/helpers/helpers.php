5ef6e1b6877c7a37d5330a172a87a935c8709578
Hashara Kumarasinghe <47107459+Hashara@users.noreply.github.com>
2019-05-25 09:53:18 +0530

    Add files via upload

---
+++ mvc/app/app/lib/helpers/helpers.php
@@ -0,0 +1,23 @@
+7<?php
+
+function dnd($data){
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+    die();
+}
+
+function sanitize($dirty){
+    return htmlentities($dirty,ENT_QUOTES,'UTF-8');
+    
+}
+
+ function currentUser($type)
+{
+    if($type=="Counselors"){
+        return Counselors::currentLoggedInUser();
+    }else if ($type=="Students"){
+        return Students::currentLoggedInUser();
+    }
+   
+}
