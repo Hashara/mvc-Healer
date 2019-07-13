f62a9da1da62eebcec0e8b1c5a3ae00eba20b216
SepaliWijerathna <47144829+SepaliWijerathna@users.noreply.github.com>
2019-07-13 08:09:45 +0530

    Add files via upload

---
+++ app/views/layouts/counsellorlayout.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title><?=$this->siteTitle("Log in");?></title>
+    <link rel="stylesheet" href="<?=PROOT?>css/css/bootstrap.css">
+    <link rel="stylesheet" href="<?=PROOT?>css/css/_variables.scss">
+    <link rel="stylesheet" href="<?=PROOT?>css/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=PROOT?>css/css/_bootswatch.scss">
+
+    <link rel="stylesheet" href="<?=PROOT?>css/style.css">
+    <link rel="stylesheet" href="<?=PROOT?>css/index.min.css">
+<link rel="stylesheet" href="<?=PROOT?>css/index.css">
+    <script src="<?=PROOT?>js/jQuery-2.2.4.min.js"></script>
+    <script src="<?=PROOT?>js/bootstrap.min.js"></script>
+    <script src="<?=PROOT?>js/ButtonMethodsProfile.js"></script>
+
+    <?= $this->content('head');?>
+  </head>
+  <body>
+
+  <?= $this->content('body');?>
+  </body>
+</html>
